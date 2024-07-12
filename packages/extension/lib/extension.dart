4470fb7b5553a519b689260a6325724608892981
ENZO <dartmolecules@gmail.com>
2024-07-12 19:48:11 +0330

    Delete packages/extension directory

--- packages/extension/lib/extension.dart
+++
@@ -1,5 +0,0 @@
-export 'src/date/date.dart';
-export 'src/date/time.dart';
-export 'src/iterable.dart';
-export 'src/num.dart';
-export 'src/string.dart';
--- packages/extension/test/all.dart
+++
@@ -1,7 +0,0 @@
-import './string.dart' as _string;
-import './date.dart' as _date;
-
-main() {
-  _string.main();
-  _date.main();
-}
